c0b9d5b217ca969a626a21c5307a8a80259c86c9
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-02-26 18:21:18 +0800

    Back school.Learning IOS by XCode and terminal first.

---
+++ Learnobjective-c/Learnobjective-c/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Learnobjective-c
+//
+//  Created by YaoxinZhuo on 2/26/16.
+//  Copyright © 2016 YaoxinZhuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
