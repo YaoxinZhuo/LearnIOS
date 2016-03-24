f43b2ea44f9670e8e16f44e1032bc50d36b75cc8
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-03-24 11:02:09 +0800

    Back learn iOS APP

---
+++ NewHelloWorld2/NewHelloWorld2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewHelloWorld2
+//
+//  Created by YaoxinZhuo on 3/24/16.
+//  Copyright © 2016 YaoxinZhuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
