da6e5c1f445e0859c7c12b099f87e29a0b4c1ff1
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-04-15 16:25:20 +0800

    finished one part of secondHomework.

---
+++ SecondHomework/04-问答题.m
@@ -0,0 +1,13 @@
+/**
+ 1、#import和#include的区别，@class代表什么？
+ 2、谈谈Object-C的内存管理方式及过程？
+ 3、Object-C有私有方法吗？私有变量呢？
+ 4、Object-C有多继承吗？没有的话用什么代替？cocoa 中所有的类都是NSObject 的子类
+ 5、内存管理 Autorelease、retain、copy、assign的set方法和含义？
+ 6、浅拷贝和深拷贝区别是什么
+ 7、C和obj-c 如何混用
+ 8、Objective-C中类别和类扩展的区别。
+ 9、我们说的Objective-C是动态运行时语言是什么意思？
+ 10、Objective-C堆和栈的区别？
+ 
+ */
