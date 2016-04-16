84f4bc65ebcbc204947489d9e07827fe4ccba3d8
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-04-16 14:47:59 +0800

    Third part of homework.

--- SecondHomework/03-编程题.m
+++ SecondHomework/03-编程题.m
@@ -1,38 +1,136 @@
 /**
- 1. 说说类
- * 文字内容
- * 图片
- * 发表时间
+ 1. 说说类              2. 作者               3. 账号
+ * 文字内容             * 名称                 * 账号名称
+ * 图片               * 生日                 * 账号密码
+ * 发表时间             * 账号                 * 账号注册时间
  * 作者
  * 转发的说说
  * 评论数
  * 转发数
- * 点赞数
- 
- 2. 作者
- * 名称
- * 生日
- * 账号
- 
- 3. 账号
- * 账号名称
- * 账号密码
- * 账号注册时间
- 
- 模拟场景：
- * 张三在2007-9-8 17：56：34的时候， 注册了一个账号（名称：itcast， 密码：123456）
- * 张三的生日是1998-7-4 18：46：24
- * 张三在2010-8-8 9：23：44的时候， 发布一条说说
- * 文字内容  @“今天心情不错”
- * 图片 @“test.png”
- * 发表时间
- * 作者
- * 转发的说说
- * 评论数 100
- * 转发数 290
- * 点赞数 2000
- 
- * 李四在2006-9-8 19：26：54的时候， 注册了一个账号（名称：lisiitcast， 密码：654321）
- * 李四的生日是1999-9-6 14：16：28
- * 李四在2011-8-8 20：47：09的时候， 转发了张三之前发布的说说， 并且还附带了一句话：@“今天心情确实不错”
- */
+ * 点赞数*/
+#import<Foundation/Foundation.h>
+@interface user: NSObject
+
+@property(nonatomic,retain) NSString *_username,*_password;
+@property(nonatomic,retain) NSDate *_registerDate;
+
+-(bool) isRegister:(NSString *)tString;//检查帐号是否注册过
+-(bool) registerUser:(NSString *)tString andPassword (NSString *)tPassword;//服务器上注册帐号
+@end
+@implementation user
+@synthesize username,password;
+@synthesize registerDate;
+-(bool) isRegister:(NSString *)tString//检查帐号是否注册过
+{
+    if(checkIsRegistered:tString)//checkIsRegistered为检查账号名是否被注册过的函数，没注册过返回true
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+-(bool) registerUser:(NSString *)tString andPassword (NSString *)tPassword
+{
+    if(isRegister(*tString))
+    {
+        [self set_username:tString];
+        [self set_password:tPassword];
+        [self set_registerDate:[NSDate date]];
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+@end
+
+
+@interface zuozhe : NSObject
+@property(nonatomic,retain) NSString *_nickName;//
+@property(nonatomic,retain) user *_user;//
+@property(nonatomic,retain) NSDate *_birthDate;//
+@end
+@implementation zuozhe
+@synthesize _nickName;
+@synthesize _user;
+@synthesize _birthDate;
+@end
+
+
+@interface shuoshuo : NSObject
+@property(nonatomic,retain) NSString *_neirong;//说说的转发文字内容
+@property(nonatomic,retain) Picture *_picture;//说说的图片
+@property(nonatomic,retain) NSDate *_publishDate;//发表时间
+@property(nonatomic,retain) zuozhe *_zuozhe;//作者
+@property(nonatomic,retain) shuoshuo *_zhuan_shuoshuo;//转发的说说
+@property(nonatomic,retain) int _pinglunshu;//评论数
+@property(nonatomic,retain) int _zhuanfashu;//转发数
+@property(nonatomic,retain) int _dianzanshu;//点赞数
+@end
+@implementation shuoshuo
+@synthesize _neirong;
+@synthesize _picture;
+@synthesize _publishDate;
+@synthesize _zuozhe;
+@synthesize _zhuan_shuoshuo;
+@synthesize _pinglunshu;
+@synthesize _dianzanshu;
+@synthesize _zhuanfashu;
+(-id) init
+{
+    if(self = [super init])
+    {
+        _pinglunhu = 0;
+        _zhuanfashu = 0;
+        _dianzanshu = 0;
+        _publishDate = [NSDate date];
+    }
+    return self;
+}
+
+@end
+
+ //模拟场景：
+user *Zhangsan = [[user alloc] init];
+[Zhangsan registerUser:@"itcast",@"123456"];
+    //张三在2007-9-8 17：56：34的时候， 注册了一个账号（名称：itcast， 密码：123456）
+zuozhe *_Zhangsan = [[zuozhe alloc] init];
+[_Zhangsan set_user:Zhangsan];
+[_Zhangsan set_birthDate: @"1998-7-4 18:46:24"];
+ //张三的生日是1998-7-4 18：46：24
+shuoshuo *Zhang_shuoshuo = [[shuoshuo alloc] init];
+[Zhang_shuoshuo set_zuozhe:_Zhangsan];
+[Zhang_shuoshuo set_publishDate:@"2010-8-8 9:23:44"];
+ //张三在2010-8-8 9：23：44的时候， 发布一条说说
+[Zhang_shuoshuo set_neirong:@"今天心情不错"];
+ //文字内容  @“今天心情不错”
+[Zhang_shuoshuo set_picture:@"test.png"];
+ //图片 @“test.png”
+//发表时间是不是和创建的时间一样？publishDate;
+ //发表时间
+[Zhang_shuoshuo set_zuozhe = _Zhangsan];
+ //作者
+[Zhang_shuoshuo set_zhuan_shuoshuo = nil];
+ //转发的说说
+[Zhang_shuoshuo._dianzanshu = 100];
+ //评论数 100
+[Zhang_shuoshuo._zhuanfashu = 200];
+ //转发数 290
+[Zhang_shuoshuo._dianzanshu = 2000];
+ //点赞数 2000
+
+user *Lisi = [[user alloc] init];
+[Lisi registerUser:@"lisitcast",@"654321"];
+ //李四在2006-9-8 19：26：54的时候， 注册了一个账号（名称：lisiitcast， 密码：654321）
+[zuozhe *_Lisi = [[zuozhe alloc] init];
+[_Lisi set_user:Lisi];
+[_Lisi set_birthDate:@"1999-9-6 14:16:28"];
+ //李四的生日是1999-9-6 14：16：28
+ shuoshuo *Lisi_shuoshuo = [[shuoshuo alloc] init];
+ [Lisi_shuoshuo set_user:_Lisi];
+ [Lisi_shuoshuo set_zhuan_shuoshuo:Zhang_shuoshuo];
+ [Lisi_shuoshuo set_publishDate:@"2011-8-8 20:47:09"];
+ //李四在2011-8-8 20：47：09的时候， 转发了张三之前发布的说说， 并且还附带了一句话：@“今天心情确实不错”*/
--- SecondHomework/SecondhomeworkTest/SecondhomeworkTest/main.m
+++ SecondHomework/SecondhomeworkTest/SecondhomeworkTest/main.m
@@ -5,41 +5,32 @@
 //  Created by YaoxinZhuo on 4/15/16.
 //  Copyright © 2016 YaoxinZhuo. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
+@class Dog;
+@interface Person : NSObject
+@property (nonatomic, retain) Dog *dog;
+@end
 
+
+@implementation Person
+- (void)dealloc
+{
+    self.dog = nil;
+    [super dealloc];
+}
+@end
+
+// 请问main函数中有无代码出现内存泄露
 int main()
 {
-    Person *p1 = nil;// p1 无值
-    
     @autoreleasepool
     {
-        p1 = [[Person alloc] init];//p1  1
+        Person *p = [[[Person alloc] init] autorelease];
+        p.dog = [[Dog alloc] init];//该Dog没有被释放
+        //Dog可以[[[Dog alloc] init] autorelease];这样添加到到外面那个autoreleasepool中去，结束了就能自动释放
+        //上面那个implementation我还有点疑问，我有点迷糊：就是时不时self.dog = nil;只是指针指向空了，但原来申请的空间是不是并初始化还没有释放，指针空了但那块内存中的东西还没走，我
+        //的疑问就是上行添加autoreleasepool后达到这样的效果。
+        [[Person alloc] init].dog = [[[Dog alloc] init] autorelease];
     }
-    
-    @autoreleasepool
-    {
-        [p1 autorelease];
-        
-        Person *p2 = [[[Person alloc] init] autorelease];
-        
-        Person *p3 = [[Person alloc] init];//p3  1
-        
-        @autoreleasepool
-        {
-            NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-            
-            [p3 autorelease];
-            
-            Person *p4 = [[Person alloc] init];
-            
-            [pool release];//p3 销毁
-            
-            [p4 autorelease];
-        }//p4销毁
-        
-    }//p1,p2销毁
-    
-    
     return 0;
 }
