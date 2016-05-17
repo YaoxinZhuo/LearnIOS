37c9b8192b86ed44ab261948f922dae964e8c15d
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-05-18 07:21:36 +0800

    .

--- UIHomework2-1/UIHomework2-1/ViewController.m
+++ UIHomework2-1/UIHomework2-1/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [NSThread sleepForTimeInterval:(5.0)];
 }
 
 - (void)didReceiveMemoryWarning {
