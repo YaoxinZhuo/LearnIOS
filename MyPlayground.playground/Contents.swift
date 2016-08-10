f09277c540c5af6f6383e9c954855825aa9d386a
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-08-10 20:13:53 +0800

    Back to learn.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,7 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let myVal1 = 1
+let myVal2 = 9.99
+var str = "Hello, playground"
---
+++ PlaygroundTest1/PlaygroundTest1/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  PlaygroundTest1
+//
+//  Created by YaoxinZhuo on 7/30/16.
+//  Copyright © 2016 YaoxinZhuo. All rights reserved.
+//
+
+import Foundation
+
+let myVariable1 = 1
+let myVariable2 = 9.99
+print("Hello, World!")
