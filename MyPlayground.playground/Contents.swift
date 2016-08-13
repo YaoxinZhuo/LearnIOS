64aca3a2ebf0e4f544c7bfe5dce68f9412b81bef
YaoxinZhuo <zhuoyaoxin@gmail.com>
2016-08-14 06:26:50 +0800

    Learn.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -16,3 +16,24 @@ default:
     let vegetableComment = "Everything tastes good in soup."
 }
 for i in 0..<4 {}
+enum Rank: Int {
+    case Ace = 1
+    case Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten
+    case Jack, Queen, King
+    func simpleDescription() -> String {
+        switch self {
+        case .Ace:
+            return "ace"
+        case .Jack:
+            return "jack"
+        case .Queen:
+            return "queen"
+        case .King:
+            return "king"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+let ace = Rank.Jack
+let aceRawValue = ace.rawValue
